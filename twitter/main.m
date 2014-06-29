25a4c55f358a186b11caf93c9cae370e45ebb84d
Alex Jose <alexnj@gmail.com>
2014-06-28 19:26:08 -0700

    Initial Commit

---
+++ twitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  twitter
+//
+//  Created by Alex on 6/28/14.
+//  Copyright (c) 2014 alexnj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
