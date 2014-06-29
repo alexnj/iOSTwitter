d5fef3536e9a004837c99c28e7aa6ba8860801f7
Alex Jose <alexnj@gmail.com>
2014-06-29 00:07:52 -0700

    Connect to Twitter button connected with Client:authorize.

--- twitter/LoginViewController.h
+++ twitter/LoginViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "TwitterClient.h"
 
 @interface LoginViewController : UIViewController
 
--- twitter/LoginViewController.m
+++ twitter/LoginViewController.m
@@ -9,6 +9,7 @@
 #import "LoginViewController.h"
 
 @interface LoginViewController ()
+- (IBAction)twitterConnectClicked:(id)sender;
 
 @end
 
@@ -35,4 +36,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)twitterConnectClicked:(id)sender {
+    [[TwitterClient sharedInstance] authorize];
+}
 @end
