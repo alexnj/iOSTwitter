fcbee3720c5ff56a88ec7f1ac891762a2c2cba45
Alex Jose <alexnj@gmail.com>
2014-06-28 19:51:11 -0700

    Initial commit

---
+++ twitter/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  twitter
+//
+//  Created by Alex on 6/28/14.
+//  Copyright (c) 2014 alexnj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ twitter/LoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.m
+//  twitter
+//
+//  Created by Alex on 6/28/14.
+//  Copyright (c) 2014 alexnj. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
