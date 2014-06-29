a43c48cf5129f1d46fc04ba10879ec3320fca7fc
Alex Jose <alexnj@gmail.com>
2014-06-28 20:44:16 -0700

    Secrets file added.

---
+++ twitter/TwitterApiSecrets.h
@@ -0,0 +1,15 @@
+//
+//  TwitterApiSecrets.h
+//  twitter
+//
+//  Created by Alex on 6/28/14.
+//  Copyright (c) 2014 alexnj. All rights reserved.
+//
+
+#ifndef twitter_TwitterApiSecrets_h
+#define twitter_TwitterApiSecrets_h
+
+NSString* const consumerKey    = @"";
+NSString* const consumerSecret = @"";
+
+#endif
